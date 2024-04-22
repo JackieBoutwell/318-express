c3fc8e1b368cca62c3c6fa0c471e212ad910ee63
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-04-22 08:30:49 -0600

    commiting lab

---
+++ ALAB-318.2.1/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+// app.use(forms.array()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//index.ejs template
+app.get("/", (req, res) => {
+  const user = { firstName: "Jackie", age: 33 };
+  res.render("pages/index", { user: user });
+  console.log(user);
+});
+
+// //about.ejs template
+app.get("/about", (req, res) => {
+  const menu = [
+    { title: "Home", href: "http://localhost:3000/" },
+    { title: "Login", href: "http://localhost:3000/login" },
+    { title: "About", href: "http://localhost:3000/about" },
+  ];
+
+  res.render("pages/about", { links: menu });
+});
+
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+})
+
+app.post("/login", (req, res) => {
+  console.log(req.body)
+  console.log('success')
+});
+
+app.get("/image", (req, res) => {
+  res.download('./image/dog.jpeg')
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
